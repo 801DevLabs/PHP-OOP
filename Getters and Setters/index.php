32fe8e51ad0bd35a25e9391dd1dfc12933c3302c
801DevLabs <801DevLabs@gmail.com>
2019-03-04 13:26:10 -0700

    updated getters and setters

--- Getters and Setters/index.php
+++ Getters and Setters/index.php
@@ -3,6 +3,11 @@
     public $name;
     public $email;
 
+    public function __construct($name, $email){
+      $this->name = $name;
+      $this->email = $email;
+    }
+
     public function setName($name){
       $this->name = $name;
     }
@@ -18,11 +23,42 @@
     public function getEmail(){
       return $this->email;
     }
+
+    // __get MAGIC METHOD
+    public function __get($property){
+      if(property_exists($this, $property)){
+        return $this->$property;
+      }
+    }
+
+    // __set MAGIC METHOD
+    public function __set($property, $value){
+      if(property_exists($this, $property)){
+        $this->$property = $value;
+      }
+      return $this;
+    }
   }
 
-  $person1 = new Person;
+  // GETTERS AND SETTERS
+  $person1 = new Person('Tom Haverford', 'tom@gmail.com');
+  echo 'Name: ' . $person1->getName() . ' Email: ' . $person1->getEmail();
 
-  $person1->setName('Tom Haverford');
-  $person1->setEmail('tom@gmail.com');
+  echo '<br>';
 
-  echo 'Name: ' . $person1->getName() . ' Email: ' . $person1->email;
+  // GET MAGIC METHOD
+  $person2 = new Person('Ron', 'ron@gmail.com');
+  echo $person2->__get('name');
+
+  echo '<br>';
+  
+  // SET MAGIC METHOD
+  $person2->__set('email', 'rons@gmail.com');
+  echo $person2->__get('email');
+
+  echo '<br>';
+
+  // CHANGE USER NAME THROUGH SETNAME
+  $person3 = new Person('Ann', 34);
+  $person3->setName('Leslie');
+  echo $person3->getName();

5ffffb0481fd8256a02e04c9b4a7f20152585506
801DevLabs <801DevLabs@gmail.com>
2019-03-04 11:31:27 -0700

    added abstraction

---
+++ Abstraction/index.php
@@ -0,0 +1,44 @@
+<?php
+  abstract class Customer {
+    // TWO CORE COMPONENTS
+    // PROPERTIES - ATTRIBUTES OF THE CLASS
+    // METHODS - FUNCTIONS WIHTIN THE CLASS
+
+    // PROPERTIES
+    protected $id;
+    protected $name;
+    protected $email;
+    protected $balance;
+
+    // CONSTRUCTORS
+    // WILL RUN EVERY TIME AN OBJECT IS INSTANTIATED
+    public function __construct($id, $name, $email, $balance){
+      $this->id = $id;
+      $this->name = $name;
+      $this->email = $email;
+      $this->balance = $balance;
+    }
+
+    // METHODS
+    abstract public function getEmail();
+  }
+
+  // $customer1 = new Customer('1', 'Tom Haverford', 'tom@gmail.com', 0);
+
+  class Subscriber extends Customer {
+    public $plan;
+
+    // CONSTRUCTOR WITH PARENT DATA
+    public function __construct($id, $name, $email, $balance, $plan){
+      parent::__construct($id, $name, $email, $balance);
+      $this->plan = $plan;
+    }
+
+    public function getEmail(){
+      return $this->email;
+    }
+  }
+
+  $subscriber = new Subscriber('1', 'Tom Haverford', 'tom@gmail.com', 0, 'Pro');
+
+  echo $subscriber->getEmail();
